e778cbe314500834a1411b2615291fc185c36999
ngthing <ngthi.nguyen21@gmail.com>
2016-11-21 16:27:47 -0500

    TimePicker Update

--- app/src/main/java/com/example/gotit/SetActivity.java
+++ app/src/main/java/com/example/gotit/SetActivity.java
@@ -17,8 +17,6 @@ import java.util.Calendar;
 
 public class SetActivity extends AppCompatActivity implements View.OnClickListener{
     Button submitButton;
-    EditText begin_et;
-    EditText end_et;
     EditText message;
     CheckBox monday;
     CheckBox tuesday;
@@ -50,22 +48,11 @@ public class SetActivity extends AppCompatActivity implements View.OnClickListen
     private void setupViews() {
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(this);
-
-        begin_et = (EditText) findViewById(R.id.begin_et);
-        end_et = (EditText) findViewById(R.id.end_et);
-
-        begin_et.setOnClickListener(this);
-        end_et.setOnClickListener(this);
-
-        begin_et.setFocusable(false);
     }
 
     //validayion
     private boolean validation(){
         message_from_edit_text =message.getText().toString();
-        start = begin_et.getText().toString();
-        end = end_et.getText().toString();
-
         if (message_from_edit_text.isEmpty() || start.isEmpty() || end.isEmpty()){
             showDialog();
             return false;
@@ -80,20 +67,6 @@ public class SetActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.submitButton:
-                //do whatever
-                break;
-            case R.id.begin_et:
-                TimePicker mTimePicker = new TimePicker();
-                mTimePicker.show(getFragmentManager(), "Select time");
-                break;
-            case R.id.end_et:
-                TimePicker mTimePicker2 = new TimePicker();
-                mTimePicker2.show(getFragmentManager(), "Select time");
-                break;
-
-        }
 
         if (v == btnTimePicker) {
 
@@ -153,21 +126,6 @@ public class SetActivity extends AppCompatActivity implements View.OnClickListen
                     }
                 });
         alertDialog.show();
-    }
-
-    public static class TimePicker extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-            return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
-        }
-        @Override
-        public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
-
-            Toast.makeText(getActivity(),"Selected Time: " + String.valueOf(hourOfDay) + " : " + String.valueOf(minute), Toast.LENGTH_LONG);
-        }
     }
 
 }
