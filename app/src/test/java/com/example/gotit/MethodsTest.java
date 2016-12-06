ecac02ce29da9b2f5f9760b40ba94d88fb9566b4
othmane <othouardi@gmail.com>
2016-12-06 10:51:23 -0500

    I put the send sms in methods class

---
+++ app/src/test/java/com/example/gotit/MethodsTest.java
@@ -0,0 +1,31 @@
+package com.example.gotit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by othmane on 12/6/2016.
+ */
+public class MethodsTest {
+    @Test
+    public void getDay() throws Exception {
+
+    }
+
+    @Test
+    public void getTime() throws Exception {
+
+    }
+
+    @Test
+    public void compareTimes() throws Exception {
+
+    }
+
+    @Test
+    public void sendAutoResponse() throws Exception {
+
+    }
+
+}
